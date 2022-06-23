7d21519ddd418e191764082119a8dd6cfedff344
Wellington Ribeiro <wellington.rib@gmail.com>
2022-06-23 12:10:39 -0300

    Update project

--- app/src/main/java/com/jwar/github_repo/App.kt
+++ app/src/main/java/com/jwar/github_repo/App.kt
@@ -7,6 +7,7 @@ import com.jwar.github_repo.core.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import timber.log.Timber
 
 class App: Application() {
@@ -15,7 +16,7 @@ class App: Application() {
         super.onCreate()
 
         startKoin{
-            androidLogger()
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@App)
             modules(dataModule + networkModule + viewModelModule)
         }
